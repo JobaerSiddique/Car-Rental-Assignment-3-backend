5bee86d8e7a5f292193148ea9d5128010f324b5a
JobaerSiddique <jobaersiddique28me@gmail.com>
2024-09-18 01:45:34 +0600

    vbbb

--- src/app.ts
+++ src/app.ts
@@ -29,12 +29,7 @@ app.use(cors({
   credentials: true // Allow cookies to be sent
 }));
 
-app.options('*', cors({
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-}));
+
 
 
 
